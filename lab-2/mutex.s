38f0812fbb8b334c4a27b2869a9c9a5d1e529dec
jarviswang <joyviwer@yahoo.com.tw>
2017-04-03 16:04:40 +0800

    Mutex Assembly Code
    Mutex lock/unlock functions implemented in arm assembly
    are shown in Lab-2, mutex.s.

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,17 +9,29 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+	mov r1, locked
+.CMP:
+	ldrex r2, [r0]
+	cmp r1, r2
+	beq .WAIT
+	strex r2, r1, [r0]
+	cmp r2, #1	@strex return 1 if not successful
+	beq .WAIT
+	dmb			@make sure the memory access above is visible
         @ END CODE INSERT
 	bx lr
+.WAIT:
+	b .CMP
 
 	.size lock_mutex, .-lock_mutex
 
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
-	@ INSERT CODE BELOW
-        
+		@ INSERT CODE BELOW
+	mov r1, unlocked
+	dmb			@make sure all access before is done
+	str r1, [r0]
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
