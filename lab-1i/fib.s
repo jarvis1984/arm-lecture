5c86e5ec6788225c49a3c336cbb3d94f44edfad8
jarviswang <joyviwer@yahoo.com.tw>
2017-04-03 14:08:36 +0800

    Fabonacci series assembly code
    The code in lab-1 is recursive,
    and the one in lab-1i is implemente with loop.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,32 +12,27 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	
+	mov r1, #-1	@previous
+	mov r2, #1	@result
+	mov r3, #0	@sum
+	mov r4, #0	@counter
+	
+.LS:
+	cmp r4, r0
+	bgt .LE
+	add r3, r1, r2
+	mov r1, r2
+	mov r2, r3
+	add r4, r4, #1
+	b .LS
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+.LE:
+	mov r0, r2
+	pop {r3, r4, r5, pc}		@EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
